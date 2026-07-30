2f5593bfd283f29900946023f11adcc99920bb78
kunalrajput9687@gmail.com <officialkunal9687@gmail.com>
2026-07-30 18:05:19 +0530

    d

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -4,7 +4,8 @@ import {
 } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000/api/auth",
+//   baseUrl: "http://localhost:5000/api/auth",
+baseUrl:"https://production-ready-multi-method.onrender.com",
   credentials: "include",
 
   prepareHeaders: (headers) => {

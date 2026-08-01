13b125b6eb54b38307a8bfe5f7b25de6cf6a0077
kunalrajput9687@gmail.com <officialkunal9687@gmail.com>
2026-08-01 15:56:51 +0530

    d

--- server/src/database/db.js
+++ server/src/database/db.js
@@ -1,17 +1,38 @@
-const mongoose = require("mongoose")
+// const mongoose = require("mongoose")
 
-const connectDB = async ()=> {
+// const connectDB = async ()=> {
 
-    try {
-        const connection = await mongoose.connect(process.env.MONGODB_URI)
-        console.log(`MongoDB connected: ${connection.connection.host}`)
-    }catch (error) {
-        console.error("mongoDB connection error:", error.message)
+//     try {
+//         const connection = await mongoose.connect(process.env.MONGODB_URI)
+//         console.log(`MongoDB connected: ${connection.connection.host}`)
+//     }catch (error) {
+//         console.error("mongoDB connection error:", error.message)
 
-        process.exit(1)
+//         process.exit(1)
 
-    }
+//     }
 
-}
+// }
 
-module.exports = connectDB
+// module.exports = connectDB
+
+
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    console.log("Connecting to MongoDB...");
+    console.log("URI Exists:", !!process.env.MONGODB_URI);
+
+    const connection = await mongoose.connect(process.env.MONGODB_URI);
+
+    console.log("MongoDB Connected:", connection.connection.host);
+  } catch (error) {
+    console.error("MongoDB Connection Error:");
+    console.error(error);
+
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;

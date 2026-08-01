1a54ec9f31b2a49d6dc5af8ba6cd40e09b859e87
kunalrajput9687@gmail.com <officialkunal9687@gmail.com>
2026-08-01 15:32:25 +0530

    d

--- server/src/services/email.service.js
+++ server/src/services/email.service.js
@@ -1,12 +1,26 @@
 const nodemailer = require("nodemailer");
 
+// const transporter = nodemailer.createTransport({
+//     service: "gmail",
+//     auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS,
+//     },
+// });
+
 const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
     },
+    connectionTimeout: 30000,
+    greetingTimeout: 30000,
+    socketTimeout: 30000,
 });
+
 
 const sendEmail = async (to, subject, html) => {
     try {
